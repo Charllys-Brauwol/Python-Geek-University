cf318f0bac102e3b859e7b2107c0bd7caf4ca492
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2022-01-20 18:52:55 -0300

    Tipo Float

---
+++ 4 - Variáveis e Tipos de Dados em Python/tipo_float.py
@@ -0,0 +1,26 @@
+"""
+Tipo Float, decimal
+
+OBS: O separador é ponto e não virgula
+"""
+valor = 1.44
+print(valor)
+print(type(valor))
+
+#Podemos converter um float para int e de int para float
+
+res = int(valor)
+print(res)
+print(type(res))
+
+res2 = float(num)
+print(res2)
+print(type(res2))
+
+#Podemos trabalhar com números complexos
+
+com = 5j
+
+print(com)
+print(type(com))
+
