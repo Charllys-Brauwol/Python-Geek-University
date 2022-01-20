ecba77e0527dc50b31cdf31fa7e229fbaf5b3ed5
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2022-01-20 18:53:04 -0300

    Tipo Inteiro

--- 4 - Variáveis e Tipos de Dados em Python/tipo_numerico.py
+++ 4 - Variáveis e Tipos de Dados em Python/tipo_numerico.py
@@ -1,36 +1,8 @@
 """
-Tipo Numerico
+Tipo Inteiro
 """
 
 num = 1_000_000
 
 print(num)
-
-"""
-Tipo Float, decimal
-
-OBS: O separador é ponto e não virgula
-"""
-valor = 1.44
-print(valor)
-print(type(valor))
-
-#Podemos converter um float para int e de int para float
-
-res = int(valor)
-print(res)
-print(type(res))
-
-res2 = float(num)
-print(res2)
-print(type(res2))
-
-#Podemos trabalhar com números complexos
-
-com = 5j
-
-print(com)
-print(type(com))
-
-
 
